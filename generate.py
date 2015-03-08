3c36b276470ced0f7ef51c6492544e958e9841ba
Scott Newman <snewman18@gmail.com>
2015-03-08 14:37:11 -0600

    Updated the template to look decent

--- generate.py
+++ generate.py
@@ -16,14 +16,31 @@ if __name__ == "__main__":
     # Turn the generated time into Eastern
     generated_at_dt = arrow.get(data['generated']).to('US/Eastern')
     generated_at = generated_at_dt.format('M/d/YYYY HH:mm:ssa') + ' ET'
-        
-    # Write the template
+
+    # Turn the nested dictionary into an easy-to-parse data structure
     delays = []
-    
+
     for item in data['airports']:
+
         if item['delay']['type'] is not None:
-            delays.append(item)
-    
+            
+            # Figure out the details
+            if item['delay']['details']['avgDelay']:
+                details = item['delay']['details']['avgDelay']
+            elif item['delay']['details']['endTime']:
+                details = item['delay']['details']['endTime']
+            else:
+                details = "N/A"
+            
+            delays.append({
+                'airport_name': item['name'],
+                'airport_code': item['iata-code'],
+                'delay_type': item['delay']['details']['type'],
+                'delay_reason': item['delay']['details']['reason'],
+                'details': details,
+            })
+
+    # Write the template
     html_output = index_template.render({
         'delays': delays,
         'generated_at': generated_at,
